a5551fc9f0c5ecb6f8cfa1084d2045b2f3f309fa
TechnoStudy <ismettechno@gmail.com>
2024-08-21 18:37:40 +0100

    sepete Ekle testi eklendi

--- src/Gun01/Merhaba2.java
+++ src/Gun01/Merhaba2.java
@@ -4,6 +4,7 @@ public class Merhaba2 {
     public static void main(String[] args) {
         System.out.println("Task 1 in diğer class daki kodlaarı");
 
+        System.out.println("Task 2 nin diğer class daki kodları");
 
     }
 }
